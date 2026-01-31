acbe3173eeeb5a5c67ead3949971c68547f524d8
mist-ic <mysticscholarate@gmail.com>
2026-01-31 17:40:23 +0530

    Fix: Restore missing imports in Main class

--- src/main/java/com/fooddelivery/FoodDeliveryApplication.java
+++ src/main/java/com/fooddelivery/FoodDeliveryApplication.java
@@ -1,3 +1,9 @@
+package com.fooddelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
